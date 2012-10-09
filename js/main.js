764528674c3b9333329f3d6993b86a356e1a74a3
Daniel Khang <dkhang@GD-24d97c.mica.edu>
2012-10-09 10:38:32 -0400

    its getting there...

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
+var jRes = jRespond([
+    {
+        label: 'handheld',
+        enter: 0,
+        exit: 750
+    },
+    {
+        label: 'desktop',
+        enter: 751,
+        exit: 100000
+    }
+]);
 
+// register enter and exit functions for a single breakpoint
+jRes.addFunc({
+    breakpoint: 'handheld',
+    enter: function() {
+	    Modernizr.load({
+		    load: 'http://code.jquery.com/mobile/1.2.0/jquery.mobile-1.2.0.min.js'
+	    });
+    },
+    exit: function() {
+    }
+});
